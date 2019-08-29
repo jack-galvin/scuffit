9a8a47c37a875433e1c021cf86f9fd1eee0dd8ad
jack <j.a.galvin19@gmail.com>
2019-08-29 17:22:58 +0100

    modified routes to support render te,mplate

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def inder():
-    return "hello"
+    user = {'username':'Jack'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'This is so scuffed!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Seriously needs patch!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
